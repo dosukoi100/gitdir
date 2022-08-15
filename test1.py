e47f263001cca52eb1c42a865d478c5718358bc4
dosukoi100 <r.yamauchi21@gmail.com>
2022-08-15 17:27:29 +0900

    重複箇所を訂正

--- test1.py
+++ test1.py
@@ -20,13 +20,4 @@ st.title('タイトルです')
 text1 = st.text_input('ここに好きな言葉を入れてください')
 st.write('あなたの好きな言葉は{}ですね?'.format(text1))
 
-#ここからStreamlitのコードを書いて行きます
-
-import streamlit as st
-
-st.title('タイトルです')
-
-text1 = st.text_input('ここに好きな言葉を入れてください')
-st.write('あなたの好きな言葉は{}ですね?'.format(text1))
-
 #プルリクエストの練習です
